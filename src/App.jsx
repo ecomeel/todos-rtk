d443c9dcf86d529453b8b339e33b1793c8f42936
ecomeel <ecomeel@gmail.com>
2023-12-01 16:38:09 +0300

    handle add todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <h1>hello</h1>
-  )
+    const [todos, setTodos] = useState([]);
+    const [text, setText] = useState("");
+
+    const addTodo = () => {
+        if (text.trim().length) {
+            setTodos([
+              ...todos,
+              {
+                id: new Date().toISOString(),
+                text,
+                completed: false
+              }
+            ]);
+            setText('');
+        }
+    };
+
+    return (
+        <div className="App">
+            <label htmlFor="">
+                <input
+                    type="text"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                />
+                <button onClick={addTodo}>Add todo</button>
+            </label>
+            <ul>
+              {
+                todos.map(todo => <li key={todo.id}>
+                  <input type="checkbox"/>
+                  <span>{todo.text}</span>
+                  <span style={{color: 'red'}}>&times;</span>
+                </li> )
+              }
+            </ul>
+        </div>
+    );
 }
 
-export default App
+export default App;
