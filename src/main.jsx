04cb60f5060e70b5feb2d0937f98fa349b8e0d37
ecomeel <ecomeel@gmail.com>
2023-12-01 17:31:43 +0300

    -set

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./store/index.js";
+import App from "./App.jsx";
+import "./index.css";
 
-const root = document.getElementById('root')
+const root = document.getElementById("root");
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
+);
